3938ce8a11d1bcac6bb5880835abcba52719b27c
RamR3R <sendtoramsundar@gmail.com>
2023-03-29 23:51:25 +0530

    Added API to Mens page

--- Scripts/mens.js
+++ Scripts/mens.js
@@ -1,1 +1,15 @@
 // link with mens.html and write the funtionalities
+
+let APIURL = `https://fine-puce-vulture-garb.cyclic.app/Dfabrica/?&_limit=5`;
+renderData(1);
+function renderData(pageNumber)
+{
+    fetch(`${APIURL}&_page=${pageNumber}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        // write the function to display the data inthe DOM.
+    })
+}
+
+// use the above part for fetching the api with pagination part
