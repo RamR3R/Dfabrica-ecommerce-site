969774275d9f0cd0463398d65abf4879d5aaaba4
Suraj12398 <surajdeosarkar99@gmail.com>
2023-03-30 22:23:58 +0530

    index done

--- Scripts/index.js
+++ Scripts/index.js
@@ -6,16 +6,21 @@
 let country=document.querySelector(".country")
 let flag=document.querySelector(".flag")
 let img=document.querySelector(".flag>img")
-
+let close=document.querySelector(".close")
+let BotCountry=document.querySelector(".botCountry")
 flag.addEventListener("click",()=>{
- country.style.display="flex";
-  
+  BotCountry.style.display="flex";
+  country.style.display="flex";
 })
-
+close.addEventListener("click",()=>{
+  BotCountry.style.display="none";
+  country.style.display="none";
+ })
 
 document.querySelector(".sc1").addEventListener("click",(e)=>{
   img.src="https://cdn-icons-png.flaticon.com/512/3909/3909444.png"
   country.style.display="none";
+  BotCountry.style.display="none";
 })
 document.querySelector(".sc2").addEventListener("click",()=>{
  img.src="https://cdn-icons-png.flaticon.com/512/197/197484.png"
