581664f78679ebeff3a32c2600dd9308eb93af87
RamR3R <sendtoramsundar@gmail.com>
2023-03-31 02:29:11 +0530

    added all the Complete CSS HTML and JS part till getting the data

--- Scripts/ramlogin.js
+++ Scripts/ramlogin.js
@@ -19,15 +19,60 @@ inputs.forEach(input => {
 	input.addEventListener("blur", remcl);
 });
 
-let login = document.getElementById("login");
-let signup = document.getElementById("signup");
+let loginbtn = document.getElementById("login");
+let signupbtn = document.getElementById("signup");
+let detailslog = document.getElementById("login-info");
+let detailssign = document.getElementById("sigup-info");
 
-login.addEventListener("click",()=>{
-    signup.classList.remove("selected");
-    login.classList.add("selected");    
+loginbtn.addEventListener("click",()=>{
+    signupbtn.classList.remove("selected");
+    loginbtn.classList.add("selected"); 
+    detailssign.classList.add("noshow");
+    detailslog.classList.remove("noshow");
+    document.getElementById("submit").value = "Login";
+
 })
 
-signup.addEventListener("click",()=>{
-    signup.classList.add("selected");
-    login.classList.remove("selected");    
+signupbtn.addEventListener("click",()=>{
+    signupbtn.classList.add("selected");
+    loginbtn.classList.remove("selected");    
+    detailslog.classList.add("noshow");
+    detailssign.classList.remove("noshow");
+    document.getElementById("submit").value = "Sign Up";
 })
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    if(document.getElementById("submit").value == "Login")
+    login();
+    else
+    signup();
+})
+
+function login(){
+    console.log("Inside login button");
+    console.log(document.getElementById("username"))
+    logincall(document.getElementById("username").value,document.getElementById("password").value);
+}
+
+function signup(){
+    console.log("Inside signup button");
+    register(document.getElementById("first-name").value,document.getElementById("second-name").value,document.getElementById("email").value,document.getElementById("signpass").value);
+}
+
+
+function logincall(user,pass){
+    console.log(user,pass);
+}
+function register(fname,lname,email,pass){
+    let obj = {
+        firstname:fname,
+        lastname : lname,
+        email:email,
+        password: pass
+    }
+
+    console.log(obj);
+}
