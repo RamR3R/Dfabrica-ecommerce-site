25fb5d954f8fb73fe09a05f2f3d6dbb05ef2a1b5
RamR3R <sendtoramsundar@gmail.com>
2023-04-01 18:18:49 +0530

    Added the auto add of details from pincode

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,12 +1,56 @@
 let userData = JSON.parse(localStorage.getItem("userdata"))||[];
 
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let mobile = document.getElementById("mobileno");
+let doorno = document.getElementById("drno");
+let street = document.getElementById("street");
+let area = document.getElementById("area");
+let pincode = document.getElementById("pincode");
+let city = document.getElementById("city");
+let state = document.getElementById("state");
+let country = document.getElementById("country");
 let formData = document.getElementById("checkout");
-
-formData.addEventListener("submit",()=>{
+rednerUserData();
+formData.addEventListener("submit",(e)=>{
+    e.preventDefault();
     console.log("form submitted");
+    renderCheckoutData();
 })
 
 
+function rednerUserData(){
+  let data = JSON.parse(localStorage.getItem("login-info"));
+  name.value = `${data.firstname} ${data.lastname}`;
+  email.value = data.email;  
+}
+
+function renderCheckoutData(){
+  let pin = pincode.value;
+  renderLocationData(pin);
+
+}
+
+function renderLocationData(pincode){
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAiLQ70ekD061TSmXQK007xKOCJHaUystk&components=postal_code:${pincode}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        console.log(data.results[0])
+        console.log(data.results[0].geometry.location)
+        let location  = data.results[0].geometry.location;
+        console.log(location.lat);
+        console.log(location.lng);
+        let l = data.results[0].address_components.length;
+        city.value = data.results[0].address_components[l-3].long_name;
+        state.value = data.results[0].address_components[l-2].long_name;
+        country.value = data.results[0].address_components[l-1].long_name;
+        // let l = document.getElementById("locate");
+
+        // l.href =`https://maps.google.com/?q=${location.lat},${location.lng}`;
+    })
+    .catch(err=>console.log(err));
+}
 
 
 
